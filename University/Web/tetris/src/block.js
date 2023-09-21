9be74d9d3835a0ad86c1d6277833590d4cd091d9
Alexsandr Proshichev <kaidux22@gmail.com>
2023-09-21 10:45:36 +0300

    Block's Classes

--- University/Web/tetris/src/block.js
+++ University/Web/tetris/src/block.js
@@ -5,8 +5,38 @@ export default class Block {
     posY = 3
 }
 
-export default class Square extends Block {
+export default class Smashboy extends Block {
     mField = [[1, 1], [1, 1]]
     mSize = 2
+}
+
+export default class Teewee extends Block {
+    mField = [[0, 0, 0], [0, 1, 0], [1, 1, 1]]
+    mSize = 3
+}
+
+export default class Hero extends Block {
+    mField = [[0, 1, 0, 0], [0, 1, 0, 0], [0, 1, 0, 0], [0, 1, 0, 0]]
+    mSize = 4
+}
+
+export default class RhodeIslandZ extends Block {
+    mField = [[0, 0, 0], [0, 1, 1], [1, 1, 0]]
+    mSize = 3
+}
+
+export default class ClevelandZ extends Block {
+    mField = [[0, 0, 0], [1, 1, 0], [0, 1, 1]]
+    mSize = 3
+}
+
+export default class BlueRicky extends Block {
+    mField = [[0, 0, 0], [1, 0, 0], [1, 1, 1]]
+    mSize = 3
+}
+
+export default class OrangeRicky extends Block {
+    mField = [[0, 0, 0], [0, 0, 1], [1, 1, 1]]
+    mSize = 3
 }
 
