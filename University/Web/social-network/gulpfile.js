1a7f572dccc8f00c0f96c699cea5d7f485efe74c
kaidux22 <kaidux22@gmail.com>
2023-10-23 02:05:36 +0300

    Main page

--- University/Web/social-network/gulpfile.js
+++ University/Web/social-network/gulpfile.js
@@ -1,4 +1,4 @@
-const {src, dest, series} = require('gulp')
+const {src, dest, parallel, series} = require('gulp')
 const pug = require('gulp-pug')
 const cleaner = require('gulp-clean')
 const babel = require('gulp-babel')
@@ -17,25 +17,25 @@ paths = {
 function pages(){
     return src(paths.pages)
         .pipe(pug())
-        .pipe(dest(paths.dest))
+        .pipe(dest(paths.dest + "pages/"))
 }
 
 function scripts(){
     return src(paths.scripts)
         .pipe(babel({presets: ['@babel/env']}))
-        .pipe(dest(paths.dest))
+        .pipe(dest(paths.dest + "scripts/"))
 }
 
 function styles(){
     return src(paths.styles)
         .pipe(less())
         .pipe(cleanCSS())
-        .pipe(dest(paths.dest))
+        .pipe(dest(paths.dest + "styles/"))
 }
 
 function sources(){
     return src(paths.sources)
-        .pipe(dest(paths.dest))
+        .pipe(dest(paths.dest + "src/"))
 }
 
 function clean(){
@@ -45,6 +45,6 @@ function clean(){
 
 
 
-exports.default = series(clean, pages, styles, scripts, sources)
+exports.default = series(clean, parallel(pages, styles, scripts, sources))
 exports.pages = pages
 exports.clean = clean
--- University/Web/social-network/routes.js
+++ University/Web/social-network/routes.js
@@ -1,54 +1,72 @@
 const router = require('express').Router()
 const path = require('path')
 const parser = require('express').urlencoded({extended: false})
+const fs = require('fs')
 
-let data = require("./Data/users.json")
+
 
 router.get("/", (req, res) => {
-    res.render("./users.html")
+    res.render("./pages/users.html")
 })
 
-router.get("/:num", (req, res) => {
-    res.render("./profile.html")
+router.get("/:file.:type", (req, res) => {
+    if(req.params.type == "css"){
+        res.sendFile(path.join(__dirname, '/views', '/styles', req.params.file + "." + req.params.type))
+    }
+    else if(req.params.type == "js"){
+        res.sendFile(path.join(__dirname, '/views', '/scripts', req.params.file + "." + req.params.type))
+    }
+    else{
+        res.sendFile(path.join(__dirname, '/views', '/src', req.params.file + "." + req.params.type))
+    }
 })
 
-router.get("/:name.svg", (req, res) => {
-    res.sendFile(path.resolve(req.params.name, ".svg"))
-})
-
-router.get("/:name.css", (req, res) => {
-    res.sendFile(path.resolve(req.params.name, ".css"))
-})
-
-router.get("/:name.js", (req, res) => {
-    res.sendFile(path.resolve(req.params.name, ".js"))
-})
-
-router.get("/favicon.ico", (req, res) => {
-    res.sendFile("./favicon.ico")
+router.get("/icon.ico", (req, res) => {
+    res.sendFile("./src/icon.ico")
 })
 
 router.post("/", parser, (req, res) => {
+    let data = require("./Data/users.json")
     if(!req.body.content){
         res.json(data)
     }
     else{
         let pattern = RegExp(req.body.content, "i");
-        console.log("Паттерн", pattern);
         let list = { "users" : []}
         
         for(let i = 0; i < data.users.length; i++){
             let string = data.users[i].lastName + " " + data.users[i].name
 
             string = string.match(pattern)
-            console.log("результат", string)
             if(string){
                 list.users.push(data.users[i])
             }
         }
 
+        res.status(202)
         res.json(list)
     }
+})
+
+router.post("/status", parser, (req, res) => {
+    let data = require("./Data/users.json")
+    let idx = Number(req.body.id)
+    let status = "confirmed"
+
+    if(data.users[idx].status == status){
+        status = "blocked"
+    }
+
+    data.users[idx].status = status;
+
+    fs.writeFile("./Data/users.json", JSON.stringify(data), (err) => {
+        if(err)
+            throw err;
+        console.log("correct!")
+    })
+
+    res.status(200)
+    res.json(data)
 })
 
 router.get("*", (req, res) => {
